0da6fb22cc3c496ac07744157a2f2d6ff7308725
shegemm5252 <49652308+shegemm5252@users.noreply.github.com>
2024-12-27 10:50:30 +0100

    add data encryption

--- KafkaConsumerTest1/obj/Debug/net8.0/KafkaConsumerTest1.AssemblyInfo.cs
+++ KafkaConsumerTest1/obj/Debug/net8.0/KafkaConsumerTest1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KafkaConsumerTest1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+420855dc9a7c2896e40b0d9d54ce2fece474d4f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b41da810f2b0725bae73fe203d685b63a6b3a8ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("KafkaConsumerTest1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KafkaConsumerTest1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SocketClientTest/obj/Debug/net8.0/SocketClientTest.AssemblyInfo.cs
+++ SocketClientTest/obj/Debug/net8.0/SocketClientTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocketClientTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+420855dc9a7c2896e40b0d9d54ce2fece474d4f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b41da810f2b0725bae73fe203d685b63a6b3a8ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocketClientTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocketClientTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
